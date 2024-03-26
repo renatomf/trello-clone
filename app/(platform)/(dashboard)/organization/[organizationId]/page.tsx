2459afdf554d464a10e59f55f4f1307920526e90
renatomf <renatomar76@gmail.com>
2024-03-26 17:32:02 -0300

    Board List

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
 import { BoardList } from "./_components/board-list";
@@ -7,8 +9,10 @@ const OrganizationIdPage = async () => {
     <div className="w-full mb-20">
       <Info />
       <Separator className="my-4" />
-      <div className="px-2">
-        <BoardList />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
