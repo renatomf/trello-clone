9be9dc53f0489bae5e249fa32101c9bd145dcd28
renatomf <renatomar76@gmail.com>
2024-03-27 12:52:52 -0300

    List component

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ListWithCards } from "@/types";
+import { ListForm } from "./list-form";
+
+interface ListContainerProps {
+  data: ListWithCards[];
+  boardId: string;
+}
+
+export const ListContainer = ({
+  data,
+  boardId
+}: ListContainerProps) => {
+
+
+  return (
+    <ol>
+      <ListForm />
+      <div className="w-1 flex-shrink-0" />
+    </ol>
+  );
+}
